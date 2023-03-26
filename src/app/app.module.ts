31f740de65c272d467fb8ae3c991623331030294
weerayut1996 <weerayut.itsd@gmail.com>
2023-03-26 23:10:47 +0700

    add_home

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
